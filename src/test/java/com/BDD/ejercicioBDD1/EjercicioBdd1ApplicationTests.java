67a8dc7d8aa78e722703381394a8f7d796eb1299
Franco Armando <francoarmando1911@gmail.com>
2025-04-23 18:46:01 -0300

    Subida inicial de archivos del proyecto

---
+++ src/test/java/com/BDD/ejercicioBDD1/EjercicioBdd1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BDD.ejercicioBDD1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioBdd1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
